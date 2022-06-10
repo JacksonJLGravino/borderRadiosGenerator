dea378cc132d1d7211349cbfd8caebdec5297dcb
Jackson Gravino <103889584+JacksonJLGravino@users.noreply.github.com>
2022-06-10 09:25:39 -0300

    refatorando o codigo

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const corners = document.querySelectorAll('.border-corner'),
+const corners = document.querySelectorAll('.change-box'),
   c1 = document.querySelector('#corner-1'),
   c2 = document.querySelector('#corner-2'),
   c3 = document.querySelector('#corner-3'),
   c4 = document.querySelector('#corner-4'),
-  hei = document.querySelector('#cornerHeight'),
-  wid = document.querySelector('#cornerWidth')
+  height = document.querySelector('#changeHeight'),
+  width = document.querySelector('#changeWidth')
 
 const box = document.querySelector('.box')
-
-const text = document.querySelector('.text')
 
 for (let i = 0; i < corners.length; i++) {
   corners[i].addEventListener('change', () => {
@@ -18,26 +16,26 @@ for (let i = 0; i < corners.length; i++) {
 
 function applyBorder() {
   let value = `${c1.value}px ${c2.value}px ${c3.value}px ${c4.value}px`
-  if (hei.value > 500) {
-    hei.value = 500
+  if (height.value > 500) {
+    height.value = 500
   }
-  if (wid.value > 500) {
-    wid.value = 500
+  if (width.value > 500) {
+    width.value = 500
   }
 
-  let heiP = `${hei.value}px`
-  let widp = `${wid.value}px`
+  let heightNew = `${height.value}px`
+  let widthNew = `${width.value}px`
   box.style.borderRadius = value
-  box.style.height = heiP
-  box.style.width = widp
-  showCode(c1.value, c2.value, c3.value, c4.value, hei.value, wid.value)
+  box.style.height = heightNew
+  box.style.width = widthNew
+  showCode(c1.value, c2.value, c3.value, c4.value, height.value, width.value)
 }
 
-function showCode(c1, c2, c3, c4, hei, wid) {
+function showCode(c1, c2, c3, c4, height, width) {
   const valorElemeto = document.getElementById('textCSS')
-  const textoUm = `border-radius: ${c1}px ${c2}px ${c3}px ${c4}px;`
-  const textoDois = `height: ${hei}px;`
-  const textoTres = `width: ${wid}px;`
+  const textOne = `border-radius: ${c1}px ${c2}px ${c3}px ${c4}px;`
+  const textTwo = `height: ${height}px;`
+  const textThree = `width: ${width}px;`
 
-  valorElemeto.innerHTML = textoUm + '\n' + textoDois + '\n' + textoTres
+  valorElemeto.innerHTML = textOne + '\n' + textTwo + '\n' + textThree
 }
